891187b98aabd431c6d4a40a2b7caba7e673bd10
Omer Gdalia <omergdalia@gmail.com>
2022-05-30 00:15:46 +0300

    Improve personnel cards

--- src/components/workshops/PersonelView.js
+++ src/components/workshops/PersonelView.js
@@ -1,23 +1,37 @@
 import React from "react";
-import { Space, Card} from 'antd';
+import { Avatar , Card, Space} from 'antd';
 
-const PersonelCard = ({name, content}) => {
+
+import etayPhoto from '../../images/personel/etay-sharon.jpg'
+import orlyPhoto from '../../images/personel/orly-kovo.jpg'
+
+const { Meta } = Card;
+
+
+const PersonelCard = ({name, content, image}) => {
     return (
-        <Card title={name} style={{ width: 300, margin: 16 }}>
-            <i>
-                {content}
-            </i>
+        <Card hoverable>
+            <Meta title={name} avatar={<Avatar src={image} size="large"/>}/>
+            <i> {content} </i>
         </Card>
     );
 }
 
 const personelInfo = [
-    {"name": "איתי שרון", "content": "שחקו ומנחה קבוצות תאטרון פלייבק,  שותף באנסמבל let’s play  ובאנסמבל  אלתרנתיב,  מנחה את קבוצת אדוות ואת קבוצת פלייבק צעירים בחיפה. דרמה תרפיסט, בוגר המכון לפסיכותרפיה בפלייבק ומומחה בשיטת אלתור טיפולי: 'טרנספורמציות התפתחותיות'"},
-    {"name": "אורלי קובו", "content": "שחקנית ומנחת סדנאות וקבוצות תאטרון פלייבק, מייסדת שותפה באנסמבל Let's Play , מנחה את קבוצת 'אספמיה', שותפה במיזם הפלייבק 'אוצרות במה' ImprovisArt עם נילי לוברני רולניק, מיזם פלייבק מיוחד המחבר את תיאטרון הפלייבק לסוגים שונים של אמנות ופרפורמנס. מנחת פלייבק מוסמכת מטעם מרכז הפלייבק העולמי CPT ."}
+    {
+        "name": "איתי שרון", 
+        "image": etayPhoto,
+        "content": "שחקו ומנחה קבוצות תאטרון פלייבק,  שותף באנסמבל let’s play  ובאנסמבל  אלתרנתיב,  מנחה את קבוצת אדוות ואת קבוצת פלייבק צעירים בחיפה. דרמה תרפיסט, בוגר המכון לפסיכותרפיה בפלייבק ומומחה בשיטת אלתור טיפולי: 'טרנספורמציות התפתחותיות'"
+    },
+    {
+        "name": "אורלי קובו",
+        "image": orlyPhoto,
+        "content": "שחקנית ומנחת סדנאות וקבוצות תאטרון פלייבק, מייסדת שותפה באנסמבל Let's Play , מנחה את קבוצת 'אספמיה', שותפה במיזם הפלייבק 'אוצרות במה' ImprovisArt עם נילי לוברני רולניק, מיזם פלייבק מיוחד המחבר את תיאטרון הפלייבק לסוגים שונים של אמנות ופרפורמנס. מנחת פלייבק מוסמכת מטעם מרכז הפלייבק העולמי CPT ."
+    }
 ]
 
-const PesonalView = () => <Space align="start">
-    {personelInfo.map(({name, content}) => <PersonelCard key={name} name={name} content={content}/>)}
+const PesonalView = () => <Space align="start" wrap>
+    {personelInfo.map((props) => <PersonelCard key={props.name} {...props} />)}
 </Space>;
 
 export default PesonalView;
