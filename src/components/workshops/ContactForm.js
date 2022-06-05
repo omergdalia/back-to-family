68feca76f5171af51915abf7743bcced2409a00c
Omer Gdalia <omergdalia@gmail.com>
2022-06-05 21:33:22 +0300

    Update contact form fields

--- src/components/workshops/ContactForm.js
+++ src/components/workshops/ContactForm.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Button, Form, Input, InputNumber } from 'antd';
+import { Button, Form, Input, InputNumber, Select } from 'antd';
+
+const { Option } = Select;
 
 const layout = {
   labelCol: {
@@ -12,42 +14,57 @@ const layout = {
 /* eslint-disable no-template-curly-in-string */
 
 const validateMessages = {
-  required: '${label} is required!',
+  required: '${label} הוא שדה חובה!',
   types: {
-    email: '${label} is not a valid email!',
-    number: '${label} is not a valid number!',
+    email: '${label} היא לא כתובת מייל תקינה!',
+    phone: '${label} הוא לא מספר טלפון תקין'
   },
   number: {
-    range: '${label} must be between ${min} and ${max}',
+    range: 'על ה${label} להיות בין ${min} ל-${max}',
   },
 };
 /* eslint-enable no-template-curly-in-string */
 
+const prefixSelector = (
+  <Form.Item name="prefix" noStyle>
+    <Select
+      style={{
+        width: 70,
+      }}
+    >
+      <Option value="972">+972</Option>
+      <Option value="1">+1</Option>
+    </Select>
+  </Form.Item>
+);
+
+
 const ContactForm = ({givenStyle}) => {
-  const onFinish = (values) => {
-    console.log(values);
-  };
-
-
   // שדות למילוי - : שם , טלפון, אימייל,  ותק בפלייבק,  הודעה :
   return (
-    <Form {...layout} style={{givenStyle}} name="nest-messages" onFinish={onFinish} validateMessages={validateMessages}>
-      <Form.Item name={['user', 'name']} label="שם" rules={[{required: true}]}>
+    <Form 
+      name="contact" 
+      method="POST" 
+      data-netlify="true" 
+      {...layout}
+      style={{givenStyle}} 
+      validateMessages={validateMessages}
+      labelWrap>
+      <Form.Item name={['user', 'name']} label="שם" required rules={[{required: true}]}>
         <Input />
       </Form.Item>
-      <Form.Item name={['user', 'email']} label="מייל" rules={[{type: 'email'}]}>
+      <Form.Item name={['user', 'phone']} label="טלפון" required rules={[{type: 'phone'}, {required: true}]}>
+        <Input addonAfter={prefixSelector} />
+      </Form.Item>
+      <Form.Item name={['user', 'email']} label="מייל" required rules={[{type: 'email'}, {required: true}]}>
         <Input />
       </Form.Item>
-      <Form.Item name={['user', 'age']} label="Age" rules={[
+      <Form.Item name={['user', 'eperience']} label="ותק בפלייבק" rules={[
           { type: 'number', min: 0, max: 99 },
-        ]}
-      >
-        <InputNumber />
+        ]}>
+        <InputNumber min={0} max={99} />
       </Form.Item>
-      <Form.Item name={['user', 'website']} label="Website">
-        <Input />
-      </Form.Item>
-      <Form.Item name={['user', 'introduction']} label="Introduction">
+      <Form.Item name={['user', 'introduction']} label="הודעה">
         <Input.TextArea />
       </Form.Item>
       <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
