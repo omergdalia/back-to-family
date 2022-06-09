b3cd9587f7cc7c30f96fa3f1217a3d572bdc81c7
Omer Gdalia <omergdalia@gmail.com>
2022-06-09 20:34:55 +0300

    small typo fixes

--- src/components/workshops/PackageView.js
+++ src/components/workshops/PackageView.js
@@ -6,14 +6,14 @@ import { MdFlightTakeoff, MdFlightLand } from 'react-icons/md';
 const PackageView = ({title, extra}) => (
     <Descriptions className='col-1' title={title} size="small" bordered>
         <Descriptions.Item label="טיסות" span={3}>
-            <MdFlightTakeoff /> טיסה ישירה להרקליון, כרתים בתאריך ה-29.9.22 בשעה 11:40 [Aegean] <br/>
-            <MdFlightLand /> טיסה ישירה מהרקליון, כרתים בתאריך ה-03.10.22 בשעה 23:50 [Aegean] <br />
+            <MdFlightTakeoff /> טיסה ישירה להרקליון, כרתים ב-29.09.22<br/>
+            <MdFlightLand /> טיסה ישירה מהרקליון, כרתים לישראל ב-03.10.22<br />
             * העברות משדה התעופה בהרקליון לריזורט, בהלוך ובחזור
         </Descriptions.Item>
         <Descriptions.Item label="לינה" span={3}>4 לילות פנסיון מלא</Descriptions.Item>
         <Descriptions.Item label="מחיר" span={3} >
             חדר זוגי - 4500 ש"ח לאדם <br />
-            חדר יחיד - 5300 ש"ח לאדם
+            חדר יחיד - 5400 ש"ח לאדם
         </Descriptions.Item>
         <Descriptions.Item label="פעילות" span={3}>
             טיול לעיר העתיקה איראפטרה וארוחת ערב בעיר <br />
