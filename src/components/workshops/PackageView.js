a237af58fc3d0405b7dcb44d776626236df9ddcf
Omer Gdalia <omergdalia@gmail.com>
2023-01-19 19:08:23 +0200

    more updates

--- src/components/workshops/PackageView.js
+++ src/components/workshops/PackageView.js
@@ -6,24 +6,25 @@ import { MdFlightTakeoff, MdFlightLand } from 'react-icons/md';
 const PackageView = ({title, extra}) => (
     <Descriptions className='col-1' title={title} size="small" bordered>
         <Descriptions.Item label="טיסות" span={3}>
-            <MdFlightTakeoff /> טיסה ישירה להרקליון, כרתים ב-19.09.23<br/>
-            <MdFlightLand /> טיסה ישירה מהרקליון, כרתים לישראל ב-23.09.23<br />
-            * העברות משדה התעופה בהרקליון לריזורט, בהלוך ובחזור
+            <MdFlightTakeoff /> טיסה ישירה להרקליון, כרתים ב-18.09.23<br/>
+            <MdFlightLand /> טיסה ישירה מהרקליון, כרתים לישראל ב-22.09.23<br />
+            * העברות משדה התעופה בהרקליון לריזורט ובחזרה
         </Descriptions.Item>
-        <Descriptions.Item label="לינה" span={3}>4 לילות פנסיון מלא</Descriptions.Item>
-        {/* <Descriptions.Item label="מחיר" span={3} >
-            חדר זוגי - ? ש"ח לאדם <br />
-            חדר יחיד - ? ש"ח לאדם
-        </Descriptions.Item> */}
+        <Descriptions.Item label="לינה" span={3}>4 לילות על בסיס פנסיון מלא (ארוחות בוקר, צהריים וערב)</Descriptions.Item>
+        <Descriptions.Item label="מחיר" span={3} >
+            חדר זוגי בוילה - 5150 ש"ח לאדם <br />
+            חדר זוגי בבקתות העץ - 5150 ש"ח לאדם<br />
+            חדר לשלישייה בדירת הסטודיו או בוילה - 4750 ש"ח לאדם
+        </Descriptions.Item>
         <Descriptions.Item label="פעילות" span={3}>
-            טיול לעיר העתיקה איראפטרה וארוחת ערב בעיר <br />
-            כל התרגולים, הסדנאות והתכנים
+            סיורים בערים עתיקות ועוד הפתעות! <br />
+            וכמובן, את כל סדנאות הפלייבק, הג'אמים והפעילויות בריזורט
         </Descriptions.Item>
         <Descriptions.Item label="לא כלול בחבילה" span={3}>
             <ul>
                 <li>ביטוח רפואי וביטוח מטען (רצוי לבצע ביטוח הכולל גם ביטול נסיעה)</li>
-                <li>הוצאות בעלות אופי אישי</li>
-                <li>ארוחות אשר לא מפורטות בתוכנית הטיול</li>
+                <li>הוצאות אישיות נוספות</li>
+                <li>ארוחות אשר לא מפורטות בתוכנית האינטנסיב</li>
                 <li>בדיקות קורונה (אם ידרשו בארץ ו/או בכרתים)</li>
             </ul>
         </Descriptions.Item>
