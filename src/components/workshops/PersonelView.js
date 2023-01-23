c2c8289eda1e29057ef075a5a1e92b275db38790
Omer Gdalia <omergdalia@gmail.com>
2023-01-23 12:34:27 +0200

    update etay's description & rename field

--- src/components/workshops/PersonelView.js
+++ src/components/workshops/PersonelView.js
@@ -6,7 +6,7 @@ import personelInfo from './personel.json';
 const { Meta } = Card;
 
 
-const PersonelCard = ({name, content, image}) => {
+const PersonelCard = ({name, description, image}) => {
     const imageSrc = useMemo(() => {
         return require(`../../images/personel/${image}`);
     }, [image])
@@ -21,7 +21,7 @@ const PersonelCard = ({name, content, image}) => {
                 size={{ xs: 64, sm: 64, md: 64, lg: 80, xl: 80, xxl: 80 }}/>
             }
         />
-            <i> {content.map((line) => <span key={line}>{line}<br /></span>)} </i>
+            <i> {description.map((line) => <span key={line}>{line}<br /></span>)} </i>
         </Card>
     );
 }
