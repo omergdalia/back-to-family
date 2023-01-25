5170ebab5c74101eb96c9bcd992cf929e4567a10
Omer Gdalia <omergdalia@gmail.com>
2023-01-25 21:01:09 +0200

    Rename galley.json --> images.json

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 import reviews from './config/reviews.json';
 import personel from './config/personel.json';
 import programs from './config/programs.json';
-import gallery from './config/gallery.json';
+import { gallery } from './config/images.json';
 
 
 const galleryImages = gallery.map((img, _) => require(`./images/resorts/${img}`));
